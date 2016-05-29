c67f9f44ee9d018d94b3dd1a55a70f6b851c6762
Alexey Noskov <alexey.noskov@gmail.com>
2016-05-29 13:18:44 +0300

    Fix metadata issues

--- metadata.rb
+++ metadata.rb
@@ -5,10 +5,12 @@ license          "MIT"
 description      "Installs Motion, a software motion detector."
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "0.1.0"
+source_url       "https://github.com/alno/chef-motion" if respond_to?(:source_url)
+issues_url       "https://github.com/alno/chef-motion" if respond_to?(:issues_url)
 
 recipe "motion",          "Set up the motion server"
 recipe "motion::monit",   "Monit config for motion server"
 
 supports "ubuntu"
 
-recommends "monit"
+depends "monit"
